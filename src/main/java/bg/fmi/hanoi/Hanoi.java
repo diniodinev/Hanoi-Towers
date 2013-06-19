b2599e133dcf0e04f733d2c2836c90e331aa442f
diniodinev <dinio.dinev@gmail.com>
2013-06-19 11:53:20 +0300

    Fix changes in code

--- src/main/java/bg/fmi/hanoi/Hanoi.java
+++ src/main/java/bg/fmi/hanoi/Hanoi.java
@@ -1,6 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Dinio Dinev.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Dinio Dinev - initial API and implementation
+ ******************************************************************************/
 package main.java.bg.fmi.hanoi;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.*;
 
 public final class Hanoi extends JFrame {
@@ -12,7 +25,8 @@ public final class Hanoi extends JFrame {
 
     private Hanoi() {
         super("Tower of Hanoi");
-
+        
+        //Put JPanels in the conteiner
         Container content = getContentPane();
         content.setBackground(Color.lightGray);
         ControlView controlArea = new ControlView();
@@ -20,6 +34,16 @@ public final class Hanoi extends JFrame {
         DrawingView drawingArea = new DrawingView();
         content.add(drawingArea, BorderLayout.EAST);
 
+        //Save the state of the game before close it with "Close button"
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.addWindowListener( new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent we) {
+                Game.storeCurrentGame();
+                System.exit(0);
+            }
+        } );
+        
         this.pack();
         this.setResizable(false);
         this.setVisible(true);
